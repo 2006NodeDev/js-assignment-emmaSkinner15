ebdea3e66c0911df555f7af0e6850c7d70f80503
benjamin skinner <benjamin.skinner15@gmail.com>
2020-06-17 15:12:03 -0400

    problems 1-8 completed. some work on 9

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -2,6 +2,37 @@
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+
+/*this solution is based on the psuedocode implementation for bubble sort
+from wikipedia: https://en.wikipedia.org/wiki/Bubble_sort#Pseudocode_implementation */
 function bubbleSort(numArray) {
-  
+    length=numArray.length
+    inOrder=true
+    for(var j=0; j<=length-1; j++){
+        if(numArray[j+1]<numArray[j]){
+            inOrder=false
+        }
+    }
+    if(inOrder){
+        return numArray
+    }else{
+        isSwapped = false
+        while(!isSwapped){
+            for(var i=1; i<=length-1; i++){
+                if(numArray[i-1] > numArray[i]){
+                    smallValue = numArray[i]
+                    bigValue = numArray[i-1]
+                    numArray[i] = bigValue
+                    numArray[i-1] = smallValue
+                    isSwapped = true
+                }
+            }
+        }
+        console.log(numArray)
+        return bubbleSort(numArray)
+    }
+
 }
+
+testArray = [100, 69, 420, 666, 1, 3, 45, 15, 38]
+console.log(bubbleSort(testArray))
--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,21 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    let {label1, label2, label3, label4, label5, label6} = someObj;
+    for(element in someObj){
+        console.log(element)
+        console.log(label1)
+    }
 }
+
+function government(counrtyName, chiefExecutive, legistlatureType, numberRepresentatives, votingRepresentatives, numberSenators){
+    this.counrtyName = counrtyName
+    this.chiefExecutive = chiefExecutive
+    this.legistlatureType = legistlatureType
+    this.numberRepresentatives = numberRepresentatives
+    this.votingRepresentatives = votingRepresentatives
+    this.numberSenators = numberSenators
+}
+let unitedStates = new government("United States", "President", "Bicameral", 438, 435, 100)
+
+objectProperties(unitedStates)
