545364b9b7b1bcfdfbf0736cd9a4e69195838000
benjamin skinner <benjamin.skinner15@gmail.com>
2020-06-16 22:59:35 -0400

    Problems 1 and 3-8 complete

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,14 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  if(someStr.length==1){
+      return true
+  }else if(someStr[0]===someStr[someStr.length-1]){
+      newStr= someStr.slice(1, someStr.length-1)
+      return isPalindrome(newStr)
+  }else{
+      return false
+  }
 }
+console.log(isPalindrome('amanaplanacanalpanama'))
+console.log(isPalindrome('Obviously not a palindome'))
--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,54 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  switch (shape) {
+    case 'Square':
+      var singleLine=""
+      for(var i=1;i<=height;++i){
+        singleLine += character
+      }
+      for(var i=1;i<=height;++i){
+        console.log(singleLine)
+      }
+      break;
+    
+    case 'Triangle':
+      var singleLine=""
+      for(var i=1;i<=height;++i){
+        singleLine += character
+        console.log(singleLine)
+      }
+      break;
+
+    case 'Diamond':
+      numberIncrement=(height-1)/2
+      for(var i=0;i<=numberIncrement; i++ ){
+        printDiamondLine(2-i, 1+2*i, character)
+      }
+      for(var i=1;i<3; i++){
+        printDiamondLine(i, -2*i+5, character)
+      }
+      break;
+
+    default:
+      console.log("Input not recognized! Please input a valid shape \n (Triangle, Diamond, or Square), and remember to capitalize the first letter")
+      break;
+  }
 }
+function printDiamondLine(numberSpaces, numberCharacters, character){
+  var singleLine=""
+  for(var i=0;i<numberSpaces; i++){
+    singleLine += " "
+  }
+  for(var i=0;i<numberCharacters; i++){
+    singleLine += character
+  }
+  for(var i=0;i<numberSpaces; i++){
+    singleLine += " "
+  }
+  console.log(singleLine)
+}
+printShape("Square", 5, "&")
+printShape("Triangle", 4, "?")
+printShape("cofefe", 5, '2')
+printShape("Diamond", 5, "A")
