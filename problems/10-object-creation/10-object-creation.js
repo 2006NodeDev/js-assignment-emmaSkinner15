0e5ec6cc32f4e2e0f4b52f74c5ea1ebdfd320a86
benjamin skinner <benjamin.skinner15@gmail.com>
2020-06-18 00:49:48 -0400

    problems 1-8, 10-11. Several need to be 2x checked

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,27 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+var wizard = {name: "Gandalf the Grey", age: 2000}
+
+function sorceror(name, age){
+        this.name = name
+        this.age = age
+}
+
+let flamel = new sorceror('Nicholas Flamel', 1500)
+
+class witch{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+}
+
+let gelinda = new witch('Gelinda the Good', 200)
+
+console.log(typeof(flamel))
+console.log(flamel)
+console.log(typeof(wizard))
+console.log(wizard)
+console.log(typeof(gelinda))
+console.log(gelinda)
--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,13 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+//is this the desired implementation?
 function spliceElement(someArr, index) {
-  
+  newArray = someArr[index]
+  someArr = someArr.splice(index,1) //is is chearing to use the already existing splice fxn?
+  return newArray
 }
+startingArray = ['Paul', 'John', 'George', 'Ringo']
+console.log(spliceElement(startingArray, 2))    //expected output: 'George'
+console.log(startingArray)  //expected output: origional array, without George
+
