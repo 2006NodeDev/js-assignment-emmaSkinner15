ac61c7284cd7410c0f14ac62f5bf7e3712a03b56
benjamin skinner <benjamin.skinner15@gmail.com>
2020-06-18 14:06:33 -0400

    1-11. #5 needs to have exceptions added to it

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,13 +2,32 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
-//is this the desired implementation?
+
+//this implementation works, but it just uses the splice fxn.
 function spliceElement(someArr, index) {
   newArray = someArr[index]
   someArr = someArr.splice(index,1) //is is cheating to use the already existing splice fxn?
   return newArray
 }
+//The code commeneted out does part of the job- it does return the spliced value,
+//but it does not remove the value from the spliced array
+/*
+function spliceElement(someArr, index){
+  arrLen = someArr.length
+  newArray = []
+  for(var i=0; i<arrLen; i++){
+    if(i!=index){
+      newArray.push(someArr[i]) 
+    }
+  }
+
+  someArr = newArray
+  console.log(someArr)
+  return someArr[index]
+}
+*/
 startingArray = ['Paul', 'John', 'George', 'Ringo']
 console.log(spliceElement(startingArray, 2))    //expected output: 'George'
+
 console.log(startingArray)  //expected output: origional array, without George
 
